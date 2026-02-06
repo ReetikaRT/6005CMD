5360a34e9c0449d53bbdead9112c2c85e89d8b72
ReetikaRT <reetika.thapar03@gmail.com>
2026-02-06 18:42:50 +0000

    feat: Week 3 cleaned boilerplate and added counter

--- week3-frontend/src/App.vue
+++ week3-frontend/src/App.vue
@@ -1,47 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+// "ref" makes a variable reactive.
+// If this changes, the HTML updates automatically.
+const count = ref(0)
+
+function increment() {
+  count.value++ // Notice: In script, we use .value
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>My Counter</h1>
+ <p>Current count: {{ count }}</p>
+ <button @click="increment">Add 1</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+  /* CSS goes here */
 </style>
