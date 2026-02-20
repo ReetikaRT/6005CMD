4c87a18db39d4b6e2401e23b971408829257e3a4
thaparr6 <thaparr6@coventry.ac.uk>
2026-02-20 14:19:45 +0000

    feat: installed ant design vue

--- week3-frontend/src/main.js
+++ week3-frontend/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+// 1. Import the library
+import Antd from 'ant-design-vue'
+// 2. Import the CSS (The styling)
+import 'ant-design-vue/dist/reset.css'
+const app = createApp(App)
+// 3. Tell Vue to use it
+app.use(Antd)
+app.mount('#app')
