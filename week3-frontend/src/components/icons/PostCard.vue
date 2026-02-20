72da1eb31e0b9c8f3aca3de65f9c18ae5187a503
thaparr6 <thaparr6@coventry.ac.uk>
2026-02-20 16:36:49 +0000

    feat: created PostCard component

---
+++ week3-frontend/src/components/icons/PostCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Card } from 'ant-design-vue';
+// Define what data this component EXPECTS to receive
+defineProps({
+ title: String,
+ summary: String,
+ imageURL: String // In JS, props are optional by default unless you specify { required: true
+}
+)
+</script>
+<template>
+    <a-card hoverable style="width: 300px">
+    <template #cover>
+    <img :alt="title" :src="imageURL || 'https://picsum.photos/300/200'" />
+</template>
+
+ <a-card-meta :title="title">
+    <template #description>
+        {{ summary }}
+    </template>
+</a-card-meta>
+</a-card>
+</template>
