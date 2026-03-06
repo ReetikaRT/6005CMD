9a4ed47c423f53b2cb9798376b4dcc5acd2ff095
thaparr6 <thaparr6@coventry.ac.uk>
2026-03-06 14:12:26 +0000

    feat: Week 7 installed router and pinia

--- week3-frontend/src/main.js
+++ week3-frontend/src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-// 1. Import the library
+import { createPinia } from 'pinia' // 1. Import Pinia
+
 import Antd from 'ant-design-vue'
-// 2. Import the CSS (The styling)
+import App from './App.vue'
+import router from './router' // 2. Import Router (We will create this file next)
+
 import 'ant-design-vue/dist/reset.css'
+
 const app = createApp(App)
-// 3. Tell Vue to use it
+
+app.use(createPinia()) // 3. Use Pinia
+app.use(router) // 4. Use Router
 app.use(Antd)
 app.mount('#app')
